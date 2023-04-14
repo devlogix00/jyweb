a1fa7863f63f927da144fff0d958a5a54fba7b11
mnyu01 <mn3305@nyu.edu>
2023-04-13 20:14:42 -0500

    bug fix

--- server.js
+++ server.js
@@ -1474,6 +1474,8 @@ app.post('/create-checkout-session', async (req, res) => {
                 },
               ],
             mode: 'payment',
+            // success_url: 'http://localhost:8080/success.html',
+            // cancel_url: 'http://localhost:8080/cancel.html',
             success_url: 'https://polar-scrubland-06961.herokuapp.com/success.html',
             cancel_url: 'https://polar-scrubland-06961.herokuapp.com/cancel.html',
             payment_intent_data: {
