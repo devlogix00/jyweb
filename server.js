0785838da27e7c31d5690fa044e8ff371893b48b
mnyu01 <mn3305@nyu.edu>
2023-04-19 14:01:17 -0500

    Server code review

--- server.js
+++ server.js
@@ -778,10 +778,10 @@ app.post('/create-checkout-session', async (req, res) => {
                 },
               ],
             mode: 'payment',
-            success_url: 'http://localhost:8080/success.html',
-            cancel_url: 'http://localhost:8080/cancel.html',
-            // success_url: 'https://polar-scrubland-06961.herokuapp.com/success.html',
-            // cancel_url: 'https://polar-scrubland-06961.herokuapp.com/cancel.html',
+            //success_url: 'http://localhost:8080/success.html',
+            //cancel_url: 'http://localhost:8080/cancel.html',
+            success_url: 'https://polar-scrubland-06961.herokuapp.com/success.html',
+            cancel_url: 'https://polar-scrubland-06961.herokuapp.com/cancel.html',
             payment_intent_data: {
                 application_fee_amount: app_fee,
                 transfer_data: {
