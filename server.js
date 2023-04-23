187509f47ae3b52535c68a9ef577ab31b7554700
mnyu01 <mn3305@nyu.edu>
2023-04-23 01:10:32 -0500

    server code review

--- server.js
+++ server.js
@@ -404,7 +404,7 @@ onValue(regList, (snapshot) => {
             onValue(loginRef, (snapshot) => {
                 const data = snapshot.val();
                 if(data === null){
-                    app.post('/analytics', async (req, res) => {
+                    app.post('/', async (req, res) => {
                         let url = req.headers.referer;
                             if(url.includes('?')){
                                // console.log('parameterized url');
@@ -479,7 +479,7 @@ onValue(drvProfileRef, (snapshot) => {
             onValue(loginRef, (snapshot) => {
                 const data = snapshot.val();
                 if(data === null){
-                    app.post('/drvanalytics', async (req, res) => {
+                    app.post('/', async (req, res) => {
                         let url = req.headers.referer;
                         if(url.includes('?')){
                         //    console.log('parameterized url');
